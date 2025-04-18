5d4d5ccb6289d8be816b00e232d30e8107b3740c
Hank-Rabbit <zhabghank1997@gmail.com>
2025-04-18 13:40:43 +1000

    Create filter context to hold filters data. Display filters condition tag although no posts. Determine whether there is Date, avoid page update state problem when setting date in api parts.

---
+++ context/FilterSelectContext.tsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useState } from "react";
+
+// Setting Type of data of `interface`
+interface FilterState {
+  platform: string;
+  sentiment: string;
+  relevance: string;
+  hasCriticism: string;
+  search: string;
+  endDate: string;
+  startDate: string;
+  sortOrder: string;
+  page: number;
+}
+
+// Setting `Context` type
+interface FilterContextType {
+  filters: FilterState;
+  setFilters: React.Dispatch<React.SetStateAction<FilterState>>;
+}
+
+// Create `Context`
+const FilterContext = createContext<FilterContextType | null>(null);
+
+// Provide Filter data
+export function FilterProvider({ children }: { children: React.ReactNode }) {
+  const [filters, setFilters] = useState<FilterState>({
+    page: 1,
+    sortOrder: "desc",
+    startDate: "",
+    endDate: "",
+    platform: "",
+    sentiment: "",
+    relevance: "",
+    hasCriticism: "",
+    search: "",
+  });
+
+  return (
+    <FilterContext.Provider value={{ filters, setFilters }}>
+      {children}
+    </FilterContext.Provider>
+  );
+}
+
+// Create Hook，Easy to get `Context`
+export function useFilters() {
+  const context = useContext(FilterContext);
+  if (!context) {
+    throw new Error("useFilters must be used within a FilterProvider");
+  }
+  return context;
+}
