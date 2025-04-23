4b5418e5bb0b90137e0601b118e7f8ecf66ba725
Rui Mao <58303736+RuiMao370@users.noreply.github.com>
2025-04-23 11:08:21 +1000

    rename website (#18)
    Co-authored-by: sagaofdemon <sagaofdemon@gmail.com>

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="mb-4 md:mb-0">
               <p className="text-gray-600 text-sm">
-                &copy; {new Date().getFullYear()} Social Media Portal. All rights reserved.
+                &copy; {new Date().getFullYear()} Hyprdata.ai Web Portal. All rights reserved.
               </p>
             </div>
             <div className="flex space-x-6">
